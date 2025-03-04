2adc3e6286ca51aeacedf5b09ea6dee3a240fef7
Tamás Tóth <tomjtoth@gmail.com>
2025-03-04 22:54:07 +0200

    init state from constructor

--- src/Board.mjs
+++ src/Board.mjs
@@ -1,10 +1,20 @@
 export class Board {
   width;
   height;
+  rows;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
+
+    this.rows = [];
+    for (let row = 0; row < height; row++) {
+      const row = [];
+      for (let col = 0; col < width; col++) {
+        row.push(".");
+      }
+      this.rows.push(row);
+    }
   }
 
   toString() {
