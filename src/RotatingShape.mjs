4c3135ec5eac79fc7027d2facc23d18eb615354b
Tamás Tóth <tomjtoth@gmail.com>
2025-03-05 00:07:15 +0200

    impl rotateRight()

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -12,4 +12,11 @@ export class RotatingShape {
   toString() {
     return this.shape.map((row) => row.join("") + "\n").join("");
   }
+
+  rotateRight() {
+    const transposed = this.shape[0].map((_, colIndex) => this.shape.map((row) => row[colIndex]));
+
+    this.shape = transposed.map((row) => row.reverse());
+    return this;
+  }
 }
