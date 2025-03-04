88108121d0d17cdecf862dfb37a28df8cb38deab
Tamás Tóth <tomjtoth@gmail.com>
2025-03-04 23:38:45 +0200

    Board.drop() throws if already falling

--- src/Board.mjs
+++ src/Board.mjs
@@ -2,10 +2,12 @@ export class Board {
   width;
   height;
   state;
+  falling;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.falling = false;
 
     this.state = [];
     for (let row = 0; row < height; row++) {
@@ -22,19 +24,26 @@ export class Board {
   }
 
   drop(char) {
+    if (this.falling) throw new Error("already falling");
+
     const col = Math.floor(this.width / 2);
     if (this.state[0][col] === ".") {
       this.state[0][col] = char;
     }
+    this.falling = true;
   }
+
   tick() {
+    let movedAnything = false;
     for (let r = this.height - 2; r >= 0; r--) {
       for (let c = 0; c < this.width; c++) {
         if (this.state[r + 1][c] === "." && this.state[r][c] !== ".") {
           this.state[r + 1][c] = this.state[r][c];
           this.state[r][c] = ".";
+          movedAnything = true;
         }
       }
     }
+    if (!movedAnything) this.falling = false;
   }
 }
